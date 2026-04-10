8e19eed6243502fa030bf16bc0fa2fc48f9b94e8
Jahnavi <jahnavichalla58@gmail.com>
2026-04-10 21:43:50 +0530

    Add files via upload

--- env.py
+++ env.py
@@ -2,6 +2,7 @@ from models import Observation, Action
 from tasks import easy, medium, hard
 import copy
 
+
 class CollegeDataEnv:
 
     def __init__(self):
@@ -12,55 +13,58 @@ class CollegeDataEnv:
         ]
         self.data = []
         self.done = False
-        self.step_count = 0
 
     def reset(self):
         self.data = copy.deepcopy(self.original_data)
         self.done = False
-        self.step_count = 0
-        return Observation(data=self.data, message="Reset done. Clean the data.")
+        return Observation(data=self.data, message="Clean college data")
 
-   def step(self, action: Action):
-    reward = 0.0
+    def step(self, action: Action):
+        reward = 0.0
 
-    if action.action_type == "remove_duplicates":
-        seen = set()
-        new_data = []
-        for d in self.data:
-            if d["id"] not in seen:
-                seen.add(d["id"])
-                new_data.append(d)
-        self.data = new_data
-        reward += 0.4
+        if action.action_type == "remove_duplicates":
+            seen = set()
+            new_data = []
+            for d in self.data:
+                if d["id"] not in seen:
+                    seen.add(d["id"])
+                    new_data.append(d)
+            self.data = new_data
+            reward += 0.4
 
-    elif action.action_type == "fill_missing":
-        for d in self.data:
-            if d["marks"] is None:
-                d["marks"] = 0
-                reward += 0.3
+        elif action.action_type == "fill_missing":
+            for d in self.data:
+                if d["marks"] is None:
+                    d["marks"] = 0
+                    reward += 0.3
 
-    elif action.action_type == "fix_format":
-        for d in self.data:
-            d["name"] = d["name"].capitalize()
-        reward += 0.3
+        elif action.action_type == "fix_format":
+            for d in self.data:
+                d["name"] = d["name"].capitalize()
+            reward += 0.3
 
-    if all(d["marks"] is not None for d in self.data):
-        self.done = True
+        
+        if all(d["marks"] is not None for d in self.data):
+            self.done = True
 
-    score_easy = easy(self.data)
-    score_medium = medium(self.data)
-    score_hard = hard(self.data)
+        
+        score_easy = easy(self.data)
+        score_medium = medium(self.data)
+        score_hard = hard(self.data)
 
-    final_score = (score_easy + score_medium + score_hard) / 3
+        final_score = (score_easy + score_medium + score_hard) / 3
 
-    return Observation(
-        data=self.data,
-        message="Step done"
-    ), reward, self.done, {
-        "score": final_score,
-        "tasks": {
-            "easy": score_easy,
-            "medium": score_medium,
-            "hard": score_hard
+        return Observation(
+            data=self.data,
+            message="Step done"
+        ), reward, self.done, {
+            "score": final_score,
+            "tasks": {
+                "easy": score_easy,
+                "medium": score_medium,
+                "hard": score_hard
+            }
         }
-    }
+
+    def get_tasks(self):
+        return ["easy", "medium", "hard"]
