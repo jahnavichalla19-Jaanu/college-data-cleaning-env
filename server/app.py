b6d42b2443309b0af38e64e19ec447c0c9828f72
Jahnavi <jahnavichalla58@gmail.com>
2026-04-11 08:37:05 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,17 +1,14 @@
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from fastapi import FastAPI
 from env import CollegeDataEnv
 from models import Action
 
 app = FastAPI()
+
 env = CollegeDataEnv()
 
 @app.get("/")
-def home():
-    return {"message": "College Data Cleaning Environment Running 🚀"}
+def root():
+    return {"message": "Running"}
 
 @app.post("/reset")
 def reset():
@@ -22,6 +19,7 @@ def step(action: Action):
     obs, reward, done, info = env.step(action)
     return {
         "data": obs.data,
+        "message": obs.message,
         "reward": reward,
         "done": done,
         "info": info
@@ -32,4 +30,4 @@ def main():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
